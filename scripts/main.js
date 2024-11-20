f5764a89a4c1ca0815f915552d208d62000d06ac
jvillos <jvillosl14@gmail.com>
2024-11-20 19:01:14 +0100

    Vite_setup

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
+import '../css/style.css'
+import javascriptLogo from '../javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { setupCounter } from '../scripts/counter'
 
 document.querySelector('#app').innerHTML = `
   <div>
