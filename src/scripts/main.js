ee57b4e5d47cb75c8b4989dd6c56c357dc382693
oligarc <oliver25399@gmail.com>
2024-11-29 13:30:15 +0100

    Revert "looks like searching is working"
    This reverts commit 60d3877508952c72d8461a81c33fed16c92bd1f7.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,12 @@
-import { fetchRecentMovies, fetchFilmsByName } from "./tmdb_api";
+import { fetchRecentMovies } from "./tmdb_api";
 
-window.onload = () => {
-  fetchRecentMovies().then((movies) => {
-    console.log("Most recent films" + movies);
-    displayMovies(movies);
-  });
-};
-
-const displayMovies = (movies) => {
+fetchRecentMovies().then((movies) => {
+  console.log("Most recent films" + movies);
   const moviesContainer = document.querySelector(".movies-container");
-  moviesContainer.innerHTML = "";
-
   const row = document.createElement("div");
   row.classList.add("row", "justify-content-between");
-
   movies.forEach((movie) => {
-    const movieCard = `
-      <div class="col-md-4 col-lg-3 mb-4">
+    const movieCard = `<div class="col-md-4 col-lg-3 mb-4">
         <div class="card">
           <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}">
           <div class="card-body">
@@ -24,24 +14,9 @@ const displayMovies = (movies) => {
             <p class="card-text">${movie.overview}</p>
           </div>
         </div>
-      </div>
-    `;
+      </div>`;
+
     row.innerHTML += movieCard;
   });
-
   moviesContainer.appendChild(row);
-};
-
-const searchButton = document.getElementById("searchFilmButton");
-const searchInput = document.getElementById("searchFilmInput");
-//Making sure we get the searchInput value
-
-searchButton.addEventListener("click", () => {
-  console.log(searchInput.value.trim());
-  if (searchInput) {
-    fetchFilmsByName(searchInput.value.trim()).then((movies) => {
-      console.log(movies);
-      displayMovies(movies);
-    });
-  }
 });
