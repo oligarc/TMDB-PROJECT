1f5f00a35b40ef77b5d9c80f8feb1dedd4946ab2
oligarc <oliver25399@gmail.com>
2024-12-10 21:50:59 +0100

    test

--- src/scripts/tmdb_api.js
+++ src/scripts/tmdb_api.js
@@ -12,10 +12,11 @@ Then in the .gitignore you have to add the .env
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_API_KEY; //esto es lo que he metido para que me funcione, si no no consigo sacar las peliculas
 
+//fetch recent movies, passing it a number of page (by default it would be 1)
 export const fetchRecentMovies = async (page = 1) => {
   try {
     const response = await fetch(
-      `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=es-ES&page=${page}`
+      `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-EN&page=${page}`
     );
     const data = await response.json();
     return data.results;
@@ -24,10 +25,11 @@ export const fetchRecentMovies = async (page = 1) => {
   }
 };
 
+//fetch films by name, passing the name and the page too
 export const fetchFilmsByName = async (searchType, page = 1) => {
   try {
     const response = await fetch(
-      `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${searchType}&language=es-ES&page=${page}`
+      `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${searchType}&language=en-EN&page=${page}`
     );
     const data = await response.json();
     console.log(data);
@@ -37,10 +39,11 @@ export const fetchFilmsByName = async (searchType, page = 1) => {
   }
 };
 
+//fetch genres
 export const fetchGenres = async () => {
   try {
     const response = await fetch(
-      `${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=es-ES`
+      `${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-ES`
     );
     const data = await response.json();
     return data.genres; // Its gonna return a list of genres with an id and a name
@@ -49,10 +52,11 @@ export const fetchGenres = async () => {
   }
 };
 
+//fetch films by genre, passing it the genreId and the page too
 export const fetchFilmsByGenre = async (genreId, page = 1) => {
   try {
     const response = await fetch(
-      `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=${genreId}&language=es-ES&page=${page}`
+      `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=${genreId}&language=en-EN&page=${page}`
     );
     const data = await response.json();
     return data.results;
@@ -61,10 +65,11 @@ export const fetchFilmsByGenre = async (genreId, page = 1) => {
   }
 };
 
+//Fetch movieInfo passing a movieID, this method returns a single object
 export const fetchMovieInfo = async (movieID) => {
   try {
     const response = await fetch(
-      `${BASE_URL}/movie/${movieID}?api_key=${API_KEY}&language=es-ES`
+      `${BASE_URL}/movie/${movieID}?api_key=${API_KEY}&language=en-EN`
     );
     const data = await response.json();
     return data; //Returns a single object
